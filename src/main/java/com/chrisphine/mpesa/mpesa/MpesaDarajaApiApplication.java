04ffd8f79314c179eb3b841c0357e892ee45881c
Chrisphine10 <ondiekchrisphine@gmail.com>
2021-06-30 01:53:58 -0700

    everything else to gert started

---
+++ src/main/java/com/chrisphine/mpesa/mpesa/MpesaDarajaApiApplication.java
@@ -0,0 +1,13 @@
+package com.chrisphine.mpesa.mpesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesaDarajaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpesaDarajaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrisphine/mpesa/mpesa/MpesaDarajaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisphine.mpesa.mpesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaDarajaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
